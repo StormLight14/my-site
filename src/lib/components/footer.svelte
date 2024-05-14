8e3b71f846fb0c9fa3c31ebbf6162e24547ab9da
StormLight14 <stormpower44@protonmail.com>
2024-05-13 20:33:49 -0700

    edit styles a lot

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,14 @@
+<footer>
+    <p>Cool Footer</p>
+</footer>
+
+<style>
+    footer {
+        padding-block: var(--size-7);
+        border-top: 1px solid var(--border);
+    }
+
+    p {
+        color: var(--text-2);
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
-    import Navbar from '$lib/components/Navbar.svelte';
-    import '$lib/styles/app.scss';
+    import Navbar from '$lib/components/navbar.svelte';
+    import Footer from '$lib/components/footer.svelte';
+
+    import '../app.scss';
+    import 'open-props/style';
+    import 'open-props/normalize';
+    import 'open-props/buttons';
 </script>
 
 <svelte:head>
@@ -10,6 +15,19 @@
 <div class="container">
     <Navbar/>
     <div>
-        <slot/>
+        <main>
+            <slot/>
+        </main>
+        <Footer/>
     </div>
 </div>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        gap: 1em;
+        min-height: 100vh;
+        max-inline-size: 1440px;
+    }
+</style>

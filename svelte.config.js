89568a89a062cd1868cddd82d54080a67c3544b8
StormLight14 <stormpower44@protonmail.com>
2024-05-16 10:33:22 -0600

    switch to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { mdsvex } from 'mdsvex';
 
 const mdsvexOptions = {
@@ -8,15 +8,7 @@ const mdsvexOptions = {
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	},
 	extensions: ['.svelte', '.md'],
 	preprocess: mdsvex(mdsvexOptions)

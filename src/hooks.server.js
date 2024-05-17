08fcb6e20b84a55a1f146ada738ba1e9d6667ba5
StormLight14 <stormpower44@protonmail.com>
2024-05-17 16:02:54 -0700

    fix smth

--- src/hooks.server.js
+++
@@ -1,9 +0,0 @@
-export const handle = (async ({ event, resolve }) => {
-    const cookieTheme = event.cookies.get("theme");
-
-    if (cookieTheme) {
-        return await resolve(event, {
-            transformPageChunk: ({ html }) => html.replace(/color-scheme="*"/, `color-scheme="${cookieTheme}"`),
-        });
-    }
-})
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,11 +19,9 @@
         <slot/>
         <Footer/>
     </div>
-    <form method="POST" action="/?/setTheme" class="theme-toggle" use:enhance>
-        <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
-        </button>
-    </form>
+    <button class="theme-toggle">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
+    </button>
 </div>
 
 <style>

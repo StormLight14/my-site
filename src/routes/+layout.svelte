8bfaf4ad7a36667279a5ed3b60d7fdeb0b78851f
StormLight14 <stormpower44@protonmail.com>
2024-05-17 16:26:20 -0700

    add theme switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import Navbar from '$lib/components/navbar.svelte';
     import Footer from '$lib/components/footer.svelte';
-    import { enhance } from '$app/forms';
+    import Toggle from '$lib/components/toggle.svelte';
 
     import '../app.scss';
     import 'open-props/style';
@@ -19,21 +19,15 @@
         <slot/>
         <Footer/>
     </div>
-    <button class="theme-toggle">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"/></svg>
-    </button>
+    <Toggle />
 </div>
 
 <style>
-    .theme-toggle {
-        float: right;
-    }
     .container {
         display: flex;
         flex-direction: row;
         gap: 1rem;
         min-height: 100vh;
-        min-width: 100%;
         max-inline-size: 1440px;
     }
 </style>

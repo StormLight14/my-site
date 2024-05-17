12ec6ab8c9c1c293f9dd44262cb874c37fe095f5
StormLight14 <stormpower44@protonmail.com>
2024-05-17 16:06:51 -0700

    Revert "move blog slug to blog directory"
    This reverts commit 4e542e5b7a06dca41442ad86b00c0ae79092815c.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,7 +9,7 @@
     <ul class="posts">
         {#each data.posts as post}
             <li class="post">
-                <a href={"blog/"}{post.slug} class="title">{post.title}</a>
+                <a href={post.slug} class="title">{post.title}</a>
                 <p class="date">{post.date}</p>
                 <p class="description">{post.description}</p>
             </li>

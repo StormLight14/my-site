963bef933422cef4d593bf8c1a2710b3c7429d02
StormLight14 <stormpower44@protonmail.com>
2024-05-17 17:13:29 -0700

    add rss feed button

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,6 +4,11 @@
 
 <h1>silly blog</h1>
 
+<div class="rss">
+	<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rss"><path d="M4 11a9 9 0 0 1 9 9"/><path d="M4 4a16 16 0 0 1 16 16"/><circle cx="5" cy="19" r="1"/></svg>
+	<a href="/rss.xml" target="_blank">RSS Feed</a>
+</div>
+
 <section class="blog">
     <ul class="posts">
         {#each data.posts as post}
@@ -17,6 +22,15 @@
 </section>
 
 <style>
+	.rss {
+		margin-top: var(--size-1);
+		display: flex;
+		flex-direction: row;
+		gap: var(--size-1);
+	}
+	.lucide-rss {
+		display: inline;
+	}
     .posts {
 		display: grid;
 		gap: 2rem;
